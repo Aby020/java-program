ae9b867cd3ab633b6012fe8aab6b8d46dab61a11
Aby <abithomas520@gmail.com>
2025-02-19 07:44:47 +0200

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,44 +1,54 @@
 import java.util.*;
-public class Product{
-int pcode;
-String pname;
-double price;
-void data(int c,String n,double p)
+class prdct
 {
-pcode=c;
-pname=n;
-price=p;
+	int pcode;
+	String pname;
+	float price;
+	
+	
 }
-void display(){
-System.out.println(pcode+"\t\t"+pname+"\t\t"+price);
-}
-static void Lowest(double price1,double price2,double price3)
+public class Product
 {
-if(price1<price2 && price1<price3)
-{
-System.out.println("Product1 has the lowest cost");
-}
-else if(price2<price1 && price2<price3)
-{
-System.out.println("Product2 has the lowest cost");
-}
-else
-{
-System.out.println("Product3 has the lowest cost");
-}
-}
-public static void main(String args[])
-{
-Product obj1=new Product();
-Product obj2=new Product();
-Product obj3=new Product();
-obj1.data(101,"Laptop",100.0);
-obj2.data(102,"AC",180.0);
-obj3.data(103,"TV",200.0);
-System.out.println("product code\tproduct name\tprice");
-obj1.display();
-obj2.display();
-obj3.display();
-Lowest(obj1.price,obj2.price,obj3.price);
-}
+	public static void main(String args[])
+	{
+		prdct obj1 = new prdct();
+		prdct obj2 = new prdct();
+		prdct obj3 = new prdct();
+		Scanner x = new Scanner(System.in);
+		Scanner y = new Scanner(System.in);
+		
+		System.out.println("\nEnter details of product 1:");
+		System.out.print("product code:");
+		obj1.pcode = x.nextInt();
+		System.out.print("product name:");
+		obj1.pname = y.nextLine();
+		System.out.print("product price:");
+		obj1.price = x.nextFloat();
+		System.out.println("\nEnter details of product 2:");
+		System.out.print("product code:");
+		obj2.pcode = x.nextInt();
+		System.out.print("product name:");
+		obj2.pname = y.nextLine();
+		System.out.print("product price:");
+		obj2.price = x.nextFloat(); 
+		System.out.println("\nEnter details of product 3:");
+		System.out.print("product code:");
+		obj3.pcode = x.nextInt();
+		System.out.print("product name:");
+		obj3.pname = y.nextLine();
+		System.out.print("product price:");
+		obj3.price = x.nextFloat();
+		if (obj1.price<obj2.price && obj1.price<obj3.price)
+		{
+			System.out.println("\nLeast price\n"+obj1.pname+" : "+obj1.price);
+		}
+		else if(obj2.price<obj1.price && obj2.price<obj3.price)
+		{
+			System.out.println("\nLeast price\n"+obj2.pname+" : "+obj2.price);
+		}
+		else
+		{
+			System.out.println("\nLeast price\n"+obj3.pname+" : "+obj3.price);		
+		}
+	}
 }
